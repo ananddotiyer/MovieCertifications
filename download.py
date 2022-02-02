0500dd637495b1c5258ebb8d3a3d41411c341a03
Anand Iyer <ananddotiyer@gmail.com>
2022-02-02 12:03:38 +0530

    Update download.py

--- download.py
+++ download.py
@@ -23,21 +23,21 @@ args = parser.parse_args()
 
 
 def get_movie_details(movie_id, lang_id):
-    url = URL.format(movie_id=movie_id, lang_id=lang_id)
-    details = {"movie_id": movie_id, "lang_id": lang_id}
-    try:
-        resp = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        return details
-    try:
-        resp.raise_for_status()
-    except requests.exceptions.HTTPError as err:  # NOQA: F841
-        return {}
+	url = URL.format(movie_id=movie_id, lang_id=lang_id)
+	details = {"movie_id": movie_id, "lang_id": lang_id}
+	try:
+		resp = requests.get(url)
+	except requests.exceptions.ConnectionError:
+		return details
+	try:
+		resp.raise_for_status()
+	except requests.exceptions.HTTPError as err:  # NOQA: F841
+		return {}
 
-    soup = BeautifulSoup(resp.content, features="lxml")
-    movie_details = [each.text for each in soup.find_all("td")[1:]]
-    details.update({k: v for k, v in zip(movie_details[::2], movie_details[1::2])})
-    return details
+	soup = BeautifulSoup(resp.content, features="lxml")
+	movie_details = [each.text for each in soup.find_all("td")[1:]]
+	details.update({k: v for k, v in zip(movie_details[::2], movie_details[1::2])})
+	return details
 
 
 movies = pd.read_csv("movies.csv", index_col=["movie_id"], squeeze=True).to_dict()
@@ -50,14 +50,22 @@ func = delayed(get_movie_details)
 
 batch_size = int(args.batch_size)
 print(f"Saving in batches of {batch_size}...")
-for batch in range(round(len(ids)/batch_size)):  #let it fail in last iteration
-    from_idx = (batch_size * batch)
-    to_idx = batch_size * (batch + 1)
-    ids_batch = ids[from_idx:to_idx]
-    try:
-        result = Parallel(n_jobs=n_jobs, verbose=10)(func(m_id, l_id) for m_id, l_id in ids_batch)
-        with open(f'movies-{from_idx}-{to_idx}.json', 'w') as fout:
-            json.dump(result, fout, indent=2)
-        print(f"Saved batch-{batch + 1} of all movies to movies-{from_idx}-{to_idx}.json")
-    except:
-        pass
+break_tag = False
+for batch in range(round(len(ids)/batch_size) + 1):	 #let it fail in last iteration
+	from_idx = batch_size * batch
+	to_idx = batch_size * (batch + 1)
+	if start + to_idx > end:
+		to_idx = end - start
+		break_tag = True
+	ids_batch = ids[from_idx:to_idx]
+	try:
+		if to_idx < from_idx + 1:
+			to_idx = from_idx + 1
+		result = Parallel(n_jobs=n_jobs, verbose=10)(func(m_id, l_id) for m_id, l_id in ids_batch)
+		with open(f'movies-{start + from_idx}-{start + to_idx}.json', 'w') as fout:
+			json.dump(result, fout, indent=2)
+		print(f"Saved batch-{batch + 1} of all movies to movies-{start + from_idx}-{start + to_idx}.json")
+		if break_tag:
+			break
+	except:
+		pass
